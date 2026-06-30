452b9de55affea14a2f0bc12083b1fbafd08269e
William Rueger <spammer.victim@gmail.com>
2026-06-30 15:24:43 -0400

    Complete rewrite
    Complete rewrite. Full reinstall git clone and with install.sh recommended.

--- uninstall.sh
+++ uninstall.sh
@@ -1,52 +1,49 @@
 #!/bin/bash
-clear
-echo "
 ##########################################################################
 # VOID_KERNEL_HOOKS Uninstall script, by William Rueger (furryfixer)
 # You must run with sudo or as root!
 ##########################################################################
-"
+
 if [ "$(id -u)" != "0" ]; then
    echo "Uninstall script must be run as root" 1>&2
    exit 1
 fi
-echo "
-This script will remove only hooks installed by Void-kernel-hooks
-from your system. Do you wish to continue?
+clear
+echo "VOID_KERNEL_HOOKS Uninstall
+
+This script will remove extra /etc/kernel.d/ hooks installed by
+Void-Kernel-Hooks. All other associated files will be removed with the
+exception of two links in /boot. These are NOT removed, because this
+could make the partition unbootable, especially wih a stripped-down
+customized \"grub.cfg\". Also, if you boot from another partition,
+running update-grub on this one will NOT make this partition bootable
+if it otherwise would not boot.
+
+For this reason, you will need to assume the responsibility of manually
+removing \"\boot\vmlinuz-linux\" and \"\boot\initramfs-linux.img\" and
+making sure your boot configuration is good. If grub uses the grub.cfg
+file from THIS partition, running of sudo update-grub after link removal
+should be sufficient, but double-check BEFORE rebooting.
+
+Do you wish to continue?
 
 Press [Y/N] and <ENTER>."
 read yn
 [[ $yn != [Yy] ]] && exit 1
 echo "
-To complete uninstall, Links must be removed from /boot directory.
-This could make the system unbootable. You may manually edit grub.cfg
-if preserving customization of that file, but there is risk. Either 
-this must be done BEFORE REBOOTING, or the system may BECOME UNBOOTABLE.
-It is strongly suggested to allow this script to update grub.cfg now.
-
-OK to create run update-grub? 
-
-Press [Y/N] and <ENTER>."
-read yn
-rm -fv /boot/vmlinuz-linux
-rm -fv /boot/initramfs-linux.img
-if [[ $yn = [Yy] ]]; then
-	update-grub
-fi
-echo "
-Uninstalling gksudo2...
+Uninstalling Void-Kernel-Hooks...
 "
-[[ -f /etc/kernel.d/post-install/30-update-links ]] && rm -fv /etc/kernel.d/post-install/30-update-links
-[[ -f /etc/kernel.d/pre-install/30-vkpurge ]] && rm -fv /etc/kernel.d/pre-install/30-vkpurge
-[[ -f /etc/kernel.d/post-remove/30-repair-links ]] && rm -fv /etc/kernel.d/post-remove/30-repair-links
-[[ -f /etc/kernel.d/pre-install/20-rm_grub ]] && rm -fv /etc/kernel.d/pre-install/20-rm_grub
-[[ -f /etc/kernel.d/pre-remove/20-rm_grub ]] && rm -fv /etc/kernel.d/pre-remove/20-rm_grub
-
-if [[ $yn = [Yy] ]]; then
-	echo "update-grub successful. If no errors, system will be bootable."
-else
-	echo "Because you did not answer \"Yes\" above, You must manually
-alter grub.cfg or run \"sudo update-grub\" BEFORE REBOOTING!"
-fi
+rm -vf /etc/kernel.d/post-install/30-update-links
+rm -vf /etc/kernel.d/pre-install/30-vkpurge
+rm -vf /etc/kernel.d/post-remove/30-repair-links
+rm -vf /etc/kernel.d/pre-install/20-rm_grub
+rm -vf /etc/kernel.d/pre-remove/20-rm_grub
+rm -vf /etc/krnl-series-default
+rm -vf /usr/local/bin/kernel-set-default
+rm -vf /usr/bin/kernel-set-default
+rm -vf /etc/knrl-series-default
 echo "
-Files installed by Void-kernel-hooks successfully removed."
+Files installed by Void-Kernel-Hooks except links in /boot successfully removed.
+
+Exiting..."
+exit 0
